170ab205fe76ad01a7c5f511e6a119f20d93117a
Guillaume De Freitas <81011333+G-DeFreitas@users.noreply.github.com>
2021-11-13 12:26:04 +0100

    Base pour la fonction Chavirement

--- MAIN/Objects/principale.d
+++
@@ -1,4 +0,0 @@
-.\objects\principale.o: MesSources\principale.c
-.\objects\principale.o: ..\Services\bordage.h
-.\objects\principale.o: ..\Services\direction.h
-.\objects\principale.o: ..\Services\Timing.h
--- Services/direction.h
+++
@@ -1,5 +0,0 @@
-#ifndef _DIRECTION_H_
-#define _DIRECTION_H_
-void direction_InitUart(void);
-void direction_Orienter_Bateau(void);
-#endif
