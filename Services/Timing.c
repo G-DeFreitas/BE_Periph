0a38d7b129064fe5a2d99bbf5d7d3e5dda559398
Guillaume De Freitas <81011333+G-DeFreitas@users.noreply.github.com>
2021-11-17 15:13:43 +0100

    Manque Juste SPI sinon Ok Simu

--- Services/Timing.c
+++ Services/Timing.c
@@ -3,11 +3,11 @@
 static MyTimer_Struct_TypeDef MonTimer;
 
 
-void Timing_Ordre(void (*InterruptionServoVoiles)(void)){
+void Timing_Ordre(void (*Interruption)(void)){
 	MonTimer.ARR = 10000-1;
 	MonTimer.PSC = 72-1;
 	MonTimer.Timer = TIM3;
 	MyTimer_Base_Init(&MonTimer);
-	MyTimer_ActiveIT(TIM3,3,InterruptionServoVoiles);
+	MyTimer_ActiveIT(TIM3,3,Interruption);
 	MyTimer_Base_Start (TIM3);
 }
--- Services/Timing.h
+++ Services/Timing.h
@@ -1,6 +1,6 @@
 #ifndef _TIMING_H_
 #define _TIMING_H_
 
-void Timing_Ordre(void (*InterruptionServoVoiles)(void));
+void Timing_Ordre(void (*Interruption)(void));
 
 #endif
